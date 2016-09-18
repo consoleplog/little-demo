e04cdb45a32335cd7431b79d03de5a33cbe5c70d
consoleplog <824923125@qq.com>
2016-09-18 10:55:02 +0800

    updata

--- src/About.js
+++ src/About.js
@@ -11,15 +11,25 @@ class About extends React.Component {
   }
   componentDidMount(){
     // console.log(searchGit)
-    searchGit().then( (data) => {console.log(data)} )
+    searchGit().then( (data) => {console.log(data);
+      this.setState({data:data.data,wait:false
+
+      })
+    } )
 
   }
   render () {
+    let gitInfo = (
+      <div>
+        <h3>{this.state.data.name}</h3>
+        <img src={this.state.data.avatar_url} />
+      </div>
+    )
     return(
       <div>
         {
           this.state.wait ? '正在获取数据' :
-          <img src={this.state.data.avatar_url} />
+          gitInfo
         }
       </div>
     )
