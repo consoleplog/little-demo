9c435f8b2e10ab789ef778f33e9b59648709b7ae
consoleplog <824923125@qq.com>
2016-09-19 22:47:09 +0800

    asd1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Routers from './routes';
 import 'highlight.js/styles/github-gist.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './main.css';
+
+import './main.css';
 
 ReactDOM.render(<Routers />, document.getElementById('app'));
