a87f25edaf4e72f0f0b90b5ddadde5438713c2ff
consoleplog <824923125@qq.com>
2016-09-19 16:42:47 +0800

    build a gh-pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routers from './routes';
-import 'highlight.js/styles/github-gist.css'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import './main.css';
-
-ReactDOM.render(<Routers />, document.getElementById('app'));
